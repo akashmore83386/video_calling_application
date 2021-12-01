ad6b24538cb8d9262ccc372bce0e816c43a93971
akashmore83386 <67957668+akashmore83386@users.noreply.github.com>
2021-12-01 23:07:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
   return (
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Video Chat</Typography>
+        <Typography variant="h2" align="center">Call to Akash More</Typography>
       </AppBar>
       <VideoPlayer />
       <Sidebar>
